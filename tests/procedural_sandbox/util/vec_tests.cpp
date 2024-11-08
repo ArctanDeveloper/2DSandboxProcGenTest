f569254fc2cac9c11b898af5ad8d6b782602dc87
ArctanDeveloper <165867213+ArctanDeveloper@users.noreply.github.com>
2024-11-08 03:36:34 +0000

    Fixed tests

--- tests/procedural_sandbox/util/vec_tests.cpp
+++ tests/procedural_sandbox/util/vec_tests.cpp
@@ -24,8 +24,8 @@ TEST(VectorTest, UnitTest) {
     EXPECT_LT(Vec2i::UNIT_Y.Magnitude(), Vec2i::UNIT_XY.Magnitude()) << "Expected (0, 1) to be less than (1, 1).";
     EXPECT_EQ(Vec2i::UNIT_X.Magnitude(), Vec2i::UNIT_Y.Magnitude()) << "Expected (1, 0) to be equal to (0, 1).";
     
-    EXPECT_GT(Vec2i::UNIT_X.Normalize().x, Vec2i::UNIT_XY.Normalize().x) << "Expected UNIT_X x to be greater than normalized UNIT_XY x.";
-    EXPECT_GT(Vec2i::UNIT_Y.Normalize().y, Vec2i::UNIT_XY.Normalize().y) << "Expected UNIT_Y y to be greater than normalized UNIT_XY y.";
+    EXPECT_EQ(Vec2i::UNIT_X.Normalize().x, Vec2i::UNIT_XY.Normalize().x) << "Expected UNIT_X x to be greater than normalized UNIT_XY x.";
+    EXPECT_EQ(Vec2i::UNIT_Y.Normalize().y, Vec2i::UNIT_XY.Normalize().y) << "Expected UNIT_Y y to be greater than normalized UNIT_XY y.";
 
 }
 
